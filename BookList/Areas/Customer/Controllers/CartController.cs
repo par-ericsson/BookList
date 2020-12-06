c2fc7b8ad8c81eb2534b799fd411117754fe1b1a
Pär Ericsson <ericsson@home.se>
2020-12-06 20:00:19 +0100

    Most of the shoppingCart finished.

---
+++ BookList/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BookList.DataAccess.Repository.IRepository;
+using BookList.Models.ViewModels;
+using BookList.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookList.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IEmailSender _emailSender;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ShoppingCartViewModel shoppingCartViewModel { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork, IEmailSender emailSender, UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _emailSender = emailSender;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            shoppingCartViewModel = new ShoppingCartViewModel()
+            {
+                OrderHeader = new Models.OrderHeader(),
+                ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "Product")
+            };
+            shoppingCartViewModel.OrderHeader.OrderTotal = 0;
+            shoppingCartViewModel.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser
+                .GetFirstOrDefault(u => u.Id == claim.Value, includeProperties: "Company");
+
+            foreach(var list in shoppingCartViewModel.ListCart)
+            {
+                list.Price = StaticDetail.GetPriceBasedOnQuantity(list.Count, list.Product.Price,
+                                                                    list.Product.Price50, list.Product.Price100);
+                shoppingCartViewModel.OrderHeader.OrderTotal += (list.Price * list.Count);
+                list.Product.Description = StaticDetail.ConvertToRawHtml(list.Product.Description);
+                if(list.Product.Description.Length > 0)
+                {
+                    list.Product.Description = list.Product.Description.Substring(0, 99) + "...";
+                }
+            }
+
+            return View(shoppingCartViewModel);
+        }
+
+        public IActionResult Plus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId, includeProperties: "Product");
+            cart.Count += 1;
+            cart.Price = StaticDetail.GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId, includeProperties: "Product");
+
+            if(cart.Count == 1)
+            {
+                var cnt = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+
+                HttpContext.Session.SetInt32(StaticDetail.ssShoppingCart, cnt - 1);
+            }
+            else
+            {
+                cart.Count -= 1;
+                cart.Price = StaticDetail.GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+
+                _unitOfWork.Save();
+            }
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(c => c.Id == cartId, includeProperties: "Product");
+
+            var cnt = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;
+
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+
+            HttpContext.Session.SetInt32(StaticDetail.ssShoppingCart, cnt - 1);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- BookList/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookList/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -191,6 +191,20 @@ namespace BookList.Areas.Identity.Pages.Account
                 }
             }
 
+            Input = new InputModel()
+            {
+                CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                RoleList = _roleManager.Roles.Where(u => u.Name != StaticDetail.Role_User_Indi).Select(x => x.Name).Select(i => new SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                }),
+            };
+
             // If we got this far, something failed, redisplay form
             return Page();
         }
